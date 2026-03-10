bb12f63e3cb040c917a45358e8c2e57b9007000b
egillax <egillax@gmail.com>
2026-03-10 16:09:45 +0100

    refactor(retriever): add retriever base and explicit in-memory names

--- src/medrap/__init__.py
+++ src/medrap/__init__.py
@@ -1,8 +1,8 @@
 from .configs import (
-    DemoTopKPayloadRetrieverConfig,
+    DemoInMemoryTopKRetrieverConfig,
+    InMemoryTopKRetrieverConfig,
     PipelineConfig,
     RAPAppConfig,
-    TopKPayloadRetrieverConfig,
     default_pipeline_config,
     float_tensor_config,
     instantiate_model,
@@ -13,7 +13,7 @@ from .heads import LinearHead
 from .model import RetrievalAugmentedModel
 from .pooling import IdentityPooling, MaskedMeanPooling
 from .query_projection import LinearQueryProjector, QueryProjector, SequenceMeanQueryProjector
-from .retrievers import TopKPayloadRetriever, build_topk_payload_retriever_from_pt
+from .retrievers import InMemoryTopKRetriever, Retriever, load_in_memory_topk_retriever_from_pt
 from .types import (
     EncoderOutput,
     FusionInput,
@@ -26,11 +26,13 @@ from .types import (
 
 __all__ = [
     "ConcatFusion",
-    "DemoTopKPayloadRetrieverConfig",
+    "DemoInMemoryTopKRetrieverConfig",
     "EncoderOutput",
     "FusionInput",
     "FusionOutput",
     "IdentityPooling",
+    "InMemoryTopKRetriever",
+    "InMemoryTopKRetrieverConfig",
     "LinearHead",
     "LinearQueryProjector",
     "MEDSCodeEncoder",
@@ -44,14 +46,13 @@ __all__ = [
     "ReplaceFusion",
     "RetrievalAugmentedModel",
     "RetrievalEncoderOutput",
+    "Retriever",
     "RetrieverOutput",
     "SequenceMeanQueryProjector",
     "TabularEncoder",
     "TokenEmbeddingEncoder",
-    "TopKPayloadRetriever",
-    "TopKPayloadRetrieverConfig",
-    "build_topk_payload_retriever_from_pt",
     "default_pipeline_config",
     "float_tensor_config",
     "instantiate_model",
+    "load_in_memory_topk_retriever_from_pt",
 ]
